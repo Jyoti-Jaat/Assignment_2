557ecf10eb327b6dc8346fb046131aeba1f0437c
Jyoti-Jaat <navdeepbhall17@gmail.com>
2025-04-15 15:18:23 +0530

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,9 @@
+#  Write a Python program that:
+# 1.   Uses a for loop to iterate over numbers from 1 to 50.
+# 2.   Calculates the sum of all integers in this range.
+# 3.   Displays the final sum.
+
+sum = 0
+for i in range(51):
+    sum = sum + i
+print("The sum of numbers from 1 to 50 is:", sum)
